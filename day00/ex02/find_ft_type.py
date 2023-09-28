86622ea0f9121f570c4c9154283432f265ef7414
viporten <viporten@student.42.fr>
2023-09-28 14:57:51 +0200

    min fix

--- day00/ex02/find_ft_type.py
+++ day00/ex02/find_ft_type.py
@@ -1,15 +1,15 @@
 def all_thing_is_obj(object: any) -> int:
-    object_type = type(object)
-    if object_type == int:
+    type_msg = {
+        str: "Brian is in the kitchen",
+        dict: "Dict",
+        set: "Set",
+        tuple: "Tuple",
+        list: "List",
+    }
+
+    msg = type_msg.get(type(object))
+    if (msg):
+        print(msg, ":", type(object))
+    else:
         print("Type not found")
-    if object_type == str:
-        print(f"Brian is in the kitchen : {object_type}")
-    if object_type == dict:
-        print(f"Dict : {object_type}")
-    if object_type == set:
-        print(f"Set : {object_type}")
-    if object_type == tuple:
-        print(f"Tuple : {object_type}")
-    if object_type == list:
-        print(f"list : {object_type}")
     return 42
