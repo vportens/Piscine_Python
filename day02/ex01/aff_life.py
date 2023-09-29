c2b7ab2f5316dbb1002096d772062d938e467ea0
Maxime <victorportenseigne@gmail.com>
2023-09-29 23:56:53 +0200

    add dataset life_expectancy_year display

---
+++ day02/ex01/aff_life.py
@@ -0,0 +1,38 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+def main():
+    data = load('../data python piscine/life_expectancy_years.csv')
+    if (data is None):
+        return
+    France = data[data['country'] == 'France']
+
+    France = France.drop(columns=['country'])
+
+    # recuperation des anneés dans une liste
+    x = France.columns.tolist()
+
+    # recuperation des valeurs dans une liste
+    y = France.iloc[0].values.tolist()
+
+    # taille du graphique
+    plt.figure(figsize=(15, 5))
+
+    # label c'est le titre, linestyle c'est pour avoir une ligne continue
+    plt.plot(x, y, label='France', linestyle='-')
+
+    # description du graphique
+    plt.title('France Life expectancy Projections')
+    plt.xlabel('Year')
+    plt.ylabel('Life expectancy')
+
+    # je precise que j'affiche les anneés tous les 50 ans
+    plt.xticks(x[::50], rotation=45)
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
