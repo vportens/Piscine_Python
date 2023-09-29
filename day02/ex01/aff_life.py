ccd8225abe17140feb354aaccb54929b13f4fb87
Maxime <victorportenseigne@gmail.com>
2023-09-30 01:46:39 +0200

    add try exception

--- day02/ex01/aff_life.py
+++ day02/ex01/aff_life.py
@@ -3,35 +3,39 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    data = load('../data python piscine/life_expectancy_years.csv')
-    if (data is None):
+    try:
+        data = load('../data python piscine/life_expectancy_years.csv')
+        if (data is None):
+            return
+        France = data[data['country'] == 'France']
+
+        France = France.drop(columns=['country'])
+
+        # recuperation des anneés dans une liste
+        x = France.columns.tolist()
+
+        # recuperation des valeurs dans une liste
+        y = France.iloc[0].values.tolist()
+
+        # taille du graphique
+        plt.figure(figsize=(15, 5))
+
+        # label c'est le titre, linestyle c'est pour avoir une ligne continue
+        plt.plot(x, y, label='France', linestyle='-')
+
+        # description du graphique
+        plt.title('France Life expectancy Projections')
+        plt.xlabel('Year')
+        plt.ylabel('Life expectancy')
+
+        # je precise que j'affiche les anneés tous les 50 ans
+        plt.xticks(x[::50], rotation=45)
+        plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+        plt.tight_layout()
+        plt.show()
+    except Exception as e:
+        print(e)
         return
-    France = data[data['country'] == 'France']
-
-    France = France.drop(columns=['country'])
-
-    # recuperation des anneés dans une liste
-    x = France.columns.tolist()
-
-    # recuperation des valeurs dans une liste
-    y = France.iloc[0].values.tolist()
-
-    # taille du graphique
-    plt.figure(figsize=(15, 5))
-
-    # label c'est le titre, linestyle c'est pour avoir une ligne continue
-    plt.plot(x, y, label='France', linestyle='-')
-
-    # description du graphique
-    plt.title('France Life expectancy Projections')
-    plt.xlabel('Year')
-    plt.ylabel('Life expectancy')
-
-    # je precise que j'affiche les anneés tous les 50 ans
-    plt.xticks(x[::50], rotation=45)
-    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-    plt.tight_layout()
-    plt.show()
 
 
 if __name__ == '__main__':
