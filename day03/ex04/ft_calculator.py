3f95132176e74a92af05c08311986a134b0c61f8
viporten <viporten@student.42.fr>
2024-01-24 18:14:08 +0100

    add docstring

--- day03/ex04/ft_calculator.py
+++ day03/ex04/ft_calculator.py
@@ -2,18 +2,29 @@ from typing import List
 
 
 class calculator:
-
+    """
+    Class calculator
+    """
     @staticmethod
     def dotproduct(V1: List[float], V2: List[float]) -> float:
+        """
+        Function product dot
+        """
         ret = sum(x*y for x, y in zip(V1, V2))
         print("Dot product is: ", ret)
 
     @staticmethod
     def add_vec(V1: List[float], V2: List[float]) -> List[float]:
+        """
+        Function add vec
+        """
         ret = [x+y for x, y in zip(V1, V2)]
         print("Add Vector is: ", ret)
 
     @staticmethod
     def sous_vec(V1: List[float], V2: List[float]) -> List[float]:
+        """
+        Function Sous vec
+        """
         ret = [x-y for x, y in zip(V1, V2)]
         print("Sous Vector is: ", ret)
