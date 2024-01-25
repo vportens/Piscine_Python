75cdb37ca8276da809de29cd9e74e263234205aa
viporten <viporten@student.42.fr>
2024-01-25 15:03:37 +0100

    add function

--- day04/ex03/new_student.py
+++ day04/ex03/new_student.py
@@ -2,9 +2,13 @@ import random
 import string
 from dataclasses import dataclass, field
 
-def generate_id() -> str:
+def generate_id():
     return "".join(random.choices(string.ascii_lowercase, k = 15))
 
 @dataclass
 class Student:
-#your code here
+    name: str
+    nickname: str
+    active: bool = True
+    id: str = generate_id()
+    login: str = genereate_login()
