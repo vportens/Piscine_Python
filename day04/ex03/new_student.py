cb4db70770982b40ff3849b908e2049dd24723fa
viporten <viporten@student.42.fr>
2024-01-25 13:58:50 +0100

    ex03 start

---
+++ day04/ex03/new_student.py
@@ -0,0 +1,10 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+def generate_id() -> str:
+    return "".join(random.choices(string.ascii_lowercase, k = 15))
+
+@dataclass
+class Student:
+#your code here
